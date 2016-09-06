e1b54af045f6e33c8fc37df809c7e36c51e83bc8
Gor <gormatevosyan14@gmail.com>
2016-09-06 17:10:36 +0400

    update

--- src/js/rodinjs/constants/AnimationTypes.js
+++ src/js/rodinjs/constants/AnimationTypes.js
@@ -1,11 +1,32 @@
 import {PARAMS} from './Params.js';
 
+const _SCALE = 'scale';
+const _POSITION = 'position';
+const _ROTATION = 'rotation';
+
 export class ANIMATION_TYPES extends PARAMS {
     constructor() {
         super();
     }
 
-    static SCALE = 'scale';
-    static POSITION = 'position';
-    static ROTATION = 'rotation';
+    /**
+     * @returns {string}
+     */
+    static get SCALE() {
+        return _SCALE;
+    }
+
+    /**
+     * @returns {string}
+     */
+    static get POSITION() {
+        return _POSITION;
+    }
+
+    /**
+     * @returns {string}
+     */
+    static get ROTATION() {
+        return _ROTATION;
+    };
 }
