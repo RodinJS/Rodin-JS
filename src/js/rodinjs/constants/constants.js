413f4c28218908f5c95eb941c02fad602dc1ca26
Gor <gormatevosyan14@gmail.com>
2016-09-07 13:42:45 +0400

    update

--- src/js/rodinjs/constants/constants.js
+++ src/js/rodinjs/constants/constants.js
@@ -1,5 +1,7 @@
 export const env_mode = "development";
 
-export * as ANIMATION_TYPES from './AnimationTypes.js';
-export * as EVENT_NAMES from './EventNames.js';
-export * as KEY_CODES from './KeyCodes.js';
+import * as ANIMATION_TYPES from './AnimationTypes.js';
+import * as EVENT_NAMES from './EventNames.js';
+import * as KEY_CODES from './KeyCodes.js';
+
+export {ANIMATION_TYPES, EVENT_NAMES, KEY_CODES};
