d27726b7c8f50c314bb02707c60a559dddcab8b5
gormat <gormatevosyan14@gmail.com>
2016-09-07 16:28:32 +0400

    timer update

--- src/js/rodinjs/utils/interval.js
+++ src/js/rodinjs/utils/interval.js
@@ -1,4 +1,10 @@
 export class Timeout {
+    /**
+     *
+     * @param {Function} cb
+     * @param {number} delay
+     * @param {boolean} autorun
+     */
     constructor(cb = function(){}, delay = 1000, autorun = false) {
         this.timer = null;
         this.run = false;
@@ -22,6 +28,9 @@ export class Timeout {
         }, this.delay);
     }
 
+    /**
+     * Start timer
+     */
     start() {
         if(this.run){
             console.warn("Timer already is running!");
@@ -31,11 +40,14 @@ export class Timeout {
         }
     }
 
+    /**
+     * Stop timer
+     */
     stop() {
         this.run = false;
     }
 
-    isRunning() {
+    get running() {
         return this.run;
     }
 }
