f4c6eda3ded943a22241128017210092a78814a2
Levon Khachatryan <kh.levon98@gmail.com>
2016-09-12 19:42:54 +0400

    socket example (incomplete)

--- examples/StateSynchronizer/index.js
+++ examples/StateSynchronizer/index.js
@@ -1,6 +1,8 @@
 import {THREE} from '../../_build/js/three/THREE.GLOBAL.js';
 import * as RODIN from '../../_build/js/rodinjs/RODIN.js';
 import {WTF} from '../../_build/js/rodinjs/RODIN.js';
+import State from '../../src/js/rodinjs/sockets/State.js';
+import Interval from '../../src/js/rodinjs/utils/interval.js';
 
 WTF.is(RODIN);
 
@@ -84,6 +86,100 @@ scene.add(light2);
 var amlight = new THREE.AmbientLight(0x222222);
 scene.add(amlight);
 
+
+var stateService = new State("room-id");
+let subscribers = [];
+let emitInterval;
+
+
+stateService.on("userenter", function (data) {
+    drawCardboardsFromSocketData(data);
+    if (data.isSelf) {
+        emitInterval = new Interval(emitStateFn, 100, true);
+    }
+});
+
+stateService.on("userleave", function (data) {
+    drawCardboardsFromSocketData(data);
+});
+
+stateService.on("userPositionChanged", function (data) {
+    for (var i = 0; i < subscribers.length; i++) {
+        if (subscribers[i].uid == data.actionUser) {
+
+            subscribers[i].cardboard.setCardboardRotation(data.data.rotation);
+            break;
+        }
+    }
+});
+
+function emitStateFn() {
+    var tmpObject = new THREE.Object3D();
+    tmpObject.applyMatrix(camera.matrixWorld);
+
+    var emitData = {
+        rotation: {
+            x: tmpObject.rotation.x,
+            y: tmpObject.rotation.y,
+            z: tmpObject.rotation.z
+        }
+    };
+
+    if (!angular.equals(emitData, stateHistory.emitData) || stateHistory.isChangeUserCount) {
+        angular.merge(stateHistory.emitData, emitData);
+        if (stateHistory.isChangeUserCount) {
+            stateHistory.isChangeUserCount = false;
+        }
+
+        stateService.emit("userPositionChanged", emitData);
+    }
+}
+
+function drawCardboardsFromSocketData(data) {
+    var maxItems = data.room.subscribers.length;
+
+    subscribers.map(function (user) {
+        scene.remove(user.cardboard.object);
+    });
+
+    subscribers.splice(0, subscribers.length);
+
+    for (let i = 0, ang = (4 * Math.PI) / maxItems; i < data.room.subscribers.length; i++, ang += (2 * Math.PI) / maxItems) {
+
+        if ($scope.UserGeneralInfo.id === data.room.subscribers[i].uid) {
+            var x = Math.round(Math.cos(ang) / 4);
+            var z = Math.round(Math.sin(ang) / 4);
+
+            cameraControls.object.position.set(x, 0, z);
+
+            continue;
+        }
+
+
+        let cardboard = new CardboardObject();
+        cardboard.on('ready', () => {
+            let x = Math.round(Math.cos(ang) / 4);
+            let z = Math.round(Math.sin(ang) / 4);
+
+            subscribers.push({
+                uid: data.room.subscribers[i].uid,
+                user: data.room.subscribers[i].user,
+                cardboard: cardboard
+            });
+
+            cardboard.object3D.position.x = x;
+            cardboard.object3D.position.z = z;
+            cardboard.object3D.scale.set(0.01, 0.01, 0.01);
+            scene.add(cardboard.object3D);
+        });
+    }
+}
+
+
+
+
+
+
 let cardboard = new CardboardObject();
 cardboard.on('ready', () => {
     cardboard.object3D.position.x = -3;
@@ -95,6 +191,20 @@ cardboard.on('ready', () => {
 cardboard.on('update', () => {
     cardboard.object3D && (cardboard.object3D.rotation.y += 0.01);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 requestAnimationFrame(animate);
 
