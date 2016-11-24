ad12365cc6d303d31187253facb1d4f43c54942b
khlevon98 <kh.levon98@gmail.com>
2016-11-24 13:37:56 +0400

    example refactoring

--- examples/ViveControls/index.js
+++ examples/ViveControls/index.js
@@ -1,79 +1,43 @@
 import {THREE} from '../../_build/js/vendor/three/THREE.GLOBAL.js';
 import * as RODIN from '../../_build/js/rodinjs/RODIN.js';
-import {WTF} from '../../_build/js/rodinjs/RODIN.js';
-
+import {SceneManager} from '../../_build/js/rodinjs/scene/SceneManager.js';
+import {CubeObject} from '../../_build/js/rodinjs/sculpt/CubeObject.js';
+import {CardboardController} from '../../_build/js/rodinjs/controllers/CardboardController.js';
 import changeParent  from '../../_build/js/rodinjs/utils/ChangeParent.js';
 
-console.log(RODIN);
 
-import '../../_build/js/vendor/three/examples/js/controls/VRControls.js';
-import '../../_build/js/vendor/three/examples/js/effects/VREffect.js';
-import '../../_build/js/vendor/three/examples/js/loaders/OBJLoader.js';
-import '../../_build/js/vendor/three/examples/js/WebVR.js';
+let scene = SceneManager.get();
+let camera = scene.camera;
+let controls = scene.controls;
+let renderer = scene.renderer;
 
-WTF.is('Rodin.JS v0.0.1');
 
-/////////////////////////////WebVR Example/////////////////////////////////////
-
-// Setup three.js WebGL renderer. Note: Antialiasing is a big performance hit.
-// Only enable it if you actually need to.
-var renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setPixelRatio(window.devicePixelRatio);
 
 renderer.shadowMap.enabled = true;
 renderer.gammaInput = true;
 renderer.gammaOutput = true;
 
-// Append the canvas element created by the renderer to document body element.
-document.body.appendChild(renderer.domElement);
 
-// Create a three.js scene.
-var scene = new THREE.Scene();
-scene.background = new THREE.Color(0x808080);
+scene.
+10000
 
-// Create a three.js camera.
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+scene.scene.background = new THREE.Color(0x808080);
 
-scene.add(camera);
 
-// Apply VR headset positional data to camera.
-var controls = new THREE.VRControls(camera);
-controls.standing = true;
-
-// Apply VR stereo rendering to renderer.
-var effect = new THREE.VREffect(renderer);
-effect.setSize(window.innerWidth, window.innerHeight);
-
-// Create a VR manager helper to enter and exit VR mode.
-var params = {
-    hideButton: false, // Default: false.
-    isUndistorted: false // Default: false.
-};
-
-var manager = new WebVRManager(renderer, effect, params);
-
-// controllers
-var controllerL = new RODIN.ViveController(RODIN.CONSTANTS.CONTROLLER_HANDS.LEFT, scene, null, 2);
+let controllerL = new RODIN.ViveController(RODIN.CONSTANTS.CONTROLLER_HANDS.LEFT, scene, null, 2);
 controllerL.standingMatrix = controls.getStandingMatrix();
-controllerL.onKeyDown = controllerKeyDown;
-controllerL.onKeyUp = controllerKeyUp;
-controllerL.onTouchUp = controllerTouchUp;
-controllerL.onTouchDown = controllerTouchDown;
-scene.add(controllerL);
+SceneManager.addController(controllerL);
 
-var controllerR = new RODIN.ViveController(RODIN.CONSTANTS.CONTROLLER_HANDS.RIGHT, scene, null, 3);
+let controllerR = new RODIN.ViveController(RODIN.CONSTANTS.CONTROLLER_HANDS.RIGHT, scene, null, 3);
 controllerR.standingMatrix = controls.getStandingMatrix();
-controllerR.onKeyDown = controllerKeyDown;
-controllerR.onKeyUp = controllerKeyUp;
-controllerR.onTouchUp = controllerTouchUp;
-controllerR.onTouchDown = controllerTouchDown;
-scene.add(controllerR);
+SceneManager.addController(controllerR);
 
-var loader = new THREE.OBJLoader();
+let loader = new THREE.OBJLoader();
 loader.setPath('./object/');
 loader.load('vr_controller_vive_1_5.obj', function (object) {
 
-    var loader = new THREE.TextureLoader();
+    let loader = new THREE.TextureLoader();
     loader.setPath('./img/');
 
     object.children[0].material.map = loader.load('onepointfive_texture.png');
@@ -83,21 +47,22 @@ loader.load('vr_controller_vive_1_5.obj', function (object) {
     controllerR.add(object.clone());
 });
 
-var geometry = new THREE.PlaneGeometry(4, 4);
-var material = new THREE.MeshStandardMaterial({
+
+
+let geometry = new THREE.PlaneGeometry(4, 4);
+let material = new THREE.MeshStandardMaterial({
     color: 0xeeeeee,
     roughness: 1.0,
     metalness: 0.0
 });
-
-var floor = new THREE.Mesh(geometry, material);
+let floor = new THREE.Mesh(geometry, material);
 floor.rotation.x = -Math.PI / 2;
 floor.receiveShadow = true;
 scene.add(floor);
 
 scene.add(new THREE.HemisphereLight(0x808080, 0x606060));
 
-var light = new THREE.DirectionalLight(0xffffff);
+let light = new THREE.DirectionalLight(0xffffff);
 light.position.set(0, 6, 0);
 light.castShadow = true;
 light.shadow.camera.top = 2;
@@ -109,20 +74,21 @@ scene.add(light);
 
 // add raycastable objects to scene
 
-var group = new THREE.Group();
+let group = new THREE.Group();
 group.position.set(1, 1, 0);
 group.rotation.y = 0.4;
 scene.add(group);
 
-var geometries = [
+let geometries = [
     new THREE.BoxGeometry(0.2, 0.2, 0.2),
     new THREE.ConeGeometry(0.2, 0.2, 64),
-    new THREE.CylinderGeometry(0.1, 0.1, 0.1, 64),
-    new THREE.IcosahedronGeometry(0.2, 1),
-    new THREE.TorusGeometry(0.2, 0.08, 12, 12)
+    new THREE.CylinderGeometry(0.2, 0.2, 0.2, 64),
+    new THREE.IcosahedronGeometry(0.2, 3),
+    new THREE.TorusGeometry(0.2, 0.04, 64, 32)
 ];
 
-for (var i = 0; i < 12; i++) {
+for (let i = 0; i < 20; i++) {
+
     let geometry = geometries[Math.floor(Math.random() * geometries.length)];
     let material = new THREE.MeshStandardMaterial({
         color: Math.random() * 0xffffff,
@@ -130,17 +96,17 @@ for (var i = 0; i < 12; i++) {
         metalness: 0.0
     });
 
-    var object = new THREE.Mesh(geometry, material);
+    let object = new THREE.Mesh(geometry, material);
 
-    object.position.x = (Math.random() - 0.5) * 2;
-    object.position.y = (Math.random() - 0.5) * 2;
-    object.position.z = (Math.random() - 0.5) * 2;
+    object.position.x = Math.random() * 4 - 2;
+    object.position.y = Math.random() * 2;
+    object.position.z = Math.random() * 4 - 2;
 
-    object.rotation.x = (Math.random() - 0.5) * 2 * Math.PI;
-    object.rotation.y = (Math.random() - 0.5) * 2 * Math.PI;
-    object.rotation.z = (Math.random() - 0.5) * 2 * Math.PI;
+    object.rotation.x = Math.random() * 2 * Math.PI;
+    object.rotation.y = Math.random() * 2 * Math.PI;
+    object.rotation.z = Math.random() * 2 * Math.PI;
 
-    object.scale.set(1, 1, 1);
+    object.scale.setScalar(Math.random() + 0.5);
 
     object.castShadow = true;
     object.receiveShadow = true;
@@ -155,171 +121,29 @@ for (var i = 0; i < 12; i++) {
 
     // hover
 
-    obj.on(RODIN.CONSTANTS.EVENT_NAMES.CONTROLLER_HOVER, (evt) => {
-        if (!obj.hoveringObjects) {
-            obj.hoveringObjects = [];
-        }
-        if (obj.hoveringObjects.indexOf(evt.controller) > -1) return;
+    obj.on(RODIN.CONSTANTS.EVENT_NAMES.CONTROLLER_HOVER, () => {
         obj.object3D.material.emissive.r = 1;
-        obj.hoveringObjects.push(evt.controller);
     });
 
-    obj.on(RODIN.CONSTANTS.EVENT_NAMES.CONTROLLER_HOVER_OUT, (evt) => {
-        if (obj.hoveringObjects.indexOf(evt.controller) > -1) {
-            obj.hoveringObjects.splice(obj.hoveringObjects.indexOf(evt.controller));
-        }
-        if (obj.hoveringObjects.length !== 0 || obj.object3D.parent !== obj.object3D.initialParent) {
-            return;
-        }
+    obj.on(RODIN.CONSTANTS.EVENT_NAMES.CONTROLLER_HOVER_OUT, () => {
         obj.object3D.material.emissive.r = 0;
     });
 
     // CONTROLLER_KEY
+    if (Math.random() > 0.5) {
+        object.material = new THREE.MeshStandardMaterial({
+            color: 0x00ff00,
+            roughness: 0.7,
+            metalness: 0.0
+        });
 
-    obj.on(RODIN.CONSTANTS.EVENT_NAMES.CONTROLLER_KEY_DOWN, (evt) => {
-        obj.object3D.scale.set(1.1, 1.1, 1.1);
-    });
+        obj.on(RODIN.CONSTANTS.EVENT_NAMES.CONTROLLER_KEY_DOWN, (evt) => {
+            evt.target.object3D.initialParent = evt.target.object3D.parent;
+            changeParent(evt.target.object3D, camera);
+        });
 
-    obj.on(RODIN.CONSTANTS.EVENT_NAMES.CONTROLLER_KEY_UP, (evt) => {
-        obj.object3D.scale.set(1, 1, 1);
-
-    });
-
-    obj.on(RODIN.CONSTANTS.EVENT_NAMES.CONTROLLER_CLICK, (evt) => {
-        if (evt.keyCode === RODIN.CONSTANTS.KEY_CODES.KEY1) {
-        }
-        if (evt.keyCode === RODIN.CONSTANTS.KEY_CODES.KEY2) {
-        }
-    });
-
-    // Controller touch
-
-    obj.on(RODIN.CONSTANTS.EVENT_NAMES.CONTROLLER_TOUCH_START, (evt) => {
-    });
-
-    obj.on(RODIN.CONSTANTS.EVENT_NAMES.CONTROLLER_TOUCH_END, (evt) => {
-    });
-
-    obj.on(RODIN.CONSTANTS.EVENT_NAMES.CONTROLLER_TAP, (evt) => {
-    });
-}
-
-function controllerKeyDown(keyCode) {
-    if (keyCode !== RODIN.CONSTANTS.KEY_CODES.KEY2) return;
-    this.engaged = true;
-    if (!this.pickedItems) {
-        this.pickedItems = [];
-    }
-
-    if (this.intersected && this.intersected.length > 0) {
-        this.intersected.map(intersect => {
-            if (intersect.object3D.parent != intersect.object3D.initialParent) {
-                return;
-            }
-
-            changeParent(intersect.object3D, this.reycastingLine);
-            //let targetParent = new THREE.Mesh(new THREE.TorusGeometry(0.1, 0.04, 12, 12));
-            let targetParent = new THREE.Object3D();
-            this.reycastingLine.add(targetParent);
-            targetParent.position.copy(intersect.object3D.position);
-            changeParent(intersect.object3D, targetParent);
-
-            this.pickedItems.push(intersect.object3D);
-            if (intersect.initialRotX) {
-                intersect.initialRotX = 0;
-                intersect.initialRotY = 0;
-            }
+        obj.on(RODIN.CONSTANTS.EVENT_NAMES.CONTROLLER_KEY_UP, (evt) => {
+            changeParent(evt.target.object3D, evt.target.object3D.initialParent);
         });
     }
-}
-
-function controllerKeyUp(keyCode) {
-    if (keyCode !== RODIN.CONSTANTS.KEY_CODES.KEY2) return;
-    this.engaged = false;
-    if (this.pickedItems && this.pickedItems.length > 0) {
-        this.pickedItems.map(item => {
-            let targetParent = item.parent;
-            changeParent(item, item.initialParent);
-            this.reycastingLine.remove(targetParent);
-        });
-        this.pickedItems = [];
-    }
-}
-
-function controllerTouchDown(keyCode, gamepad) {
-
-    if (!this.engaged || keyCode !== RODIN.CONSTANTS.KEY_CODES.KEY1) return;
-
-    if (this.intersected && this.intersected.length > 0) {
-        this.intersected.map(intersect => {
-            if (!gamepad.initialTouchX && gamepad.initialTouchX != 0) {
-                gamepad.initialTouchX = -gamepad.axes[1];
-                gamepad.initialTouchY = -gamepad.axes[0];
-            }
-
-            if (!intersect.initialRotX && intersect.initialRotX != 0) {
-
-                intersect.initialRotX = +intersect.object3D.parent.rotation.x;
-                intersect.initialRotY = +intersect.object3D.parent.rotation.y;
-            }
-            let x = (intersect.initialRotX + ((-gamepad.axes[1]) - gamepad.initialTouchX));
-            let y = (intersect.initialRotY + ((-gamepad.axes[0]) - gamepad.initialTouchY));
-
-            let directionY = new THREE.Vector3(0, 1, 0).normalize();
-            let quaternionY = new THREE.Quaternion();
-            quaternionY.setFromAxisAngle(directionY, -y);
-
-            let directionX = new THREE.Vector3(1, 0, 0).normalize();
-            let quaternionX = new THREE.Quaternion();
-            quaternionX.setFromAxisAngle(directionX, x);
-
-            intersect.object3D.parent.updateMatrixWorld();
-            intersect.object3D.parent.quaternion.copy(new THREE.Quaternion().multiplyQuaternions(quaternionX, quaternionY));
-        });
-    }
-}
-
-function controllerTouchUp(keyCode, gamepad) {
-    if (keyCode !== RODIN.CONSTANTS.KEY_CODES.KEY1) return;
-    if (this.intersected && this.intersected.length > 0) {
-        this.intersected.map(intersect => {
-            gamepad.initialTouchX = null;
-            gamepad.initialTouchZ = null;
-            intersect.initialRotX = 0;
-            intersect.initialRotY = 0;
-
-            let holderObj = intersect.object3D.parent;
-            changeParent(intersect.object3D, intersect.object3D.initialParent);
-            holderObj.rotation.set(0, 0, 0);
-            changeParent(intersect.object3D, holderObj);
-        });
-    }
-}
-
-// Kick off animation loop
-requestAnimationFrame(animate);
-
-window.addEventListener('resize', onResize, true);
-window.addEventListener('vrdisplaypresentchange', onResize, true);
-
-// Request animation frame loop function
-function animate(timestamp) {
-
-    // Update controller.
-    controllerL.update();
-    controllerR.update();
-
-    // Update VR headset position and apply to camera.
-    controls.update();
-
-    // Render the scene through the manager.
-    manager.render(scene, camera, timestamp);
-
-    requestAnimationFrame(animate);
-}
-
-function onResize(e) {
-    effect.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
 }
