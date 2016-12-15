18f02be4cf0ae169bad73f481d88cb34f5bf77e3
gormat <gormatevosyan14@gmail.com>
2016-12-15 18:00:45 +0400

    incomplete

--- src/js/rodinjs/controllers/gamePads/GamePad.js
+++ src/js/rodinjs/controllers/gamePads/GamePad.js
@@ -388,7 +388,7 @@ export class GamePad extends THREE.Object3D {
         }
     }
 
-    set touchDown (value) {
+    set touchUp (value) {
         throw new ErrorProtectedFieldChange('touchUp');
     }
 
--- src/js/rodinjs/sculpt/ModelLoader.js
+++ src/js/rodinjs/sculpt/ModelLoader.js
@@ -1,8 +1,8 @@
-import {ColladaModelObject} from './ColladaModelObject.js';
+import {ColladaModelObject} from './ModelObject/ColladaModelObject.js';
 import {FBXModelObject} from './FBXModelObject.js';
 import {OBJModelObject} from './OBJModelObject.js';
 import {JSONModelObject} from './JSONModelObject.js';
-import {JDModelObject} from './JDModelObject.js';
+import {JDModelObject} from './ModelObject/JDModelObject.js';
 import {ErrorInstantiationFailed, ErrorUnsupportedModelType} from '../error/CustomErrors.js';
 
 const supportedTypes = {
@@ -14,11 +14,24 @@ const supportedTypes = {
     'jd': JDModelObject
 };
 
+/**
+ * Class ModelLoader
+ * Loads 3D models .DAE .FBX .OBJ .JSON .JS .JD formats
+ */
 export class ModelLoader {
+    /**
+     * throws error.
+     * use static method load
+     */
     constructor() {
         throw new ErrorInstantiationFailed('ModelLoader');
     }
 
+    /**
+     * load model form url, create Sculpt object
+     * @param {string} url - url for model
+     * @returns {Sculpt} - created sculpt object from model
+     */
     static load(url = '') {
         const urlSplitted = url.split('.');
         const type = urlSplitted[urlSplitted.length - 1].toLowerCase();
