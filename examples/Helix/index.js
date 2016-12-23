65a0bd7f015220201914780e275057565214e425
gormat <gormatevosyan14@gmail.com>
2016-12-23 17:07:38 +0400

    fix

--- examples/Helix/index.js
+++ examples/Helix/index.js
@@ -4,6 +4,7 @@ import {SceneManager} from '../../_build/js/rodinjs/scene/SceneManager.js';
 import {MouseController} from '../../_build/js/rodinjs/controllers/MouseController.js';
 import {MouseGamePad} from '../../_build/js/rodinjs/controllers/gamePads/MouseGamePad.js';
 import {Element} from '../../_build/js/rodinjs/sculpt/elements/Element.js';
+import {THREEObject} from '../../_build/js/rodinjs/sculpt/THREEObject.js';
 import {ANIMATION_TYPES} from '../../_build/js/rodinjs/constants/constants.js';
 import {Animation} from '../../_build/js/rodinjs/animation/Animation.js';
 import {EVENT_NAMES} from '../../_build/js/rodinjs/constants/constants.js';
@@ -53,11 +54,12 @@ helix.addThumb = function (thumb) {
 
 helix.on('ready', () => {
     helix.object3D.position.z = -1;
+    helix.object3D.position.y = scene.controls.userHeight;
     scene.add(helix.object3D);
     helix.concentrate(0);
 });
 
-class HelixThumb extends Element {
+class HelixThumb extends THREEObject {
     constructor (thumbParams) {
         let params = {
             width: 0.9,
@@ -70,12 +72,45 @@ class HelixThumb extends Element {
                 position: { h: 50, v: 50 }
             }
         };
-        super(params);
+
+        super(new THREE.Object3D());
+
+        this.thumb = new Element(params);
+        this.thumb.on('ready', () => {
+            this.object3D.add(this.thumb.object3D);
+            this.thumb.raycastable = true;
+            this.thumb.forceHover = true;
+        });
+
+        this.thumb.on('update', () => {
+            let currentAlpha = this.currentAlpha || 0;
+            currentAlpha = currentAlpha + (this.alpha - currentAlpha) / RODIN.Time.deltaTime();
+            const alpha = Math.max(-1, Math.min(currentAlpha, 1));
+            this.thumb.object3D.material.opacity = 1 - Math.abs(alpha);
+
+            if (this.uv) {
+                let currentUV = this.currentUV || { x: 0, y: 0 };
+                currentUV.x = currentUV.x + (this.uv.x - currentUV.x) / RODIN.Time.deltaTime();
+                currentUV.y = currentUV.y + (this.uv.y - currentUV.y) / RODIN.Time.deltaTime();
+                this.thumb.object3D.rotation.y = (currentUV.x - 0.5) / 4;
+                this.thumb.object3D.rotation.x = (0.5 - currentUV.y) / 2;
+                this.currentUV = currentUV;
+            }
+        });
+
+        this.thumb.on(EVENT_NAMES.CONTROLLER_HOVER, (evt) => {
+            this.uv = evt.uv;
+        });
+
+        this.thumb.on(EVENT_NAMES.CONTROLLER_HOVER_OUT, () => {
+            this.uv = { x: .5, y: .5 }
+        });
+
+        this.thumb.on(EVENT_NAMES.CONTROLLER_KEY_DOWN, (evt) => {
+            this.hasOwnProperty("index") && helix.concentrate(this.index);
+        });
 
         this.on('ready', () => {
-            this.forceHover = true;
-            this.object3D.position.y = scene.controls.userHeight;
-            this.raycastable = true;
             helix.object3D.add(this.object3D);
             this.animator.add(animations.hover);
             this.animator.add(animations.hoverOut);
@@ -87,34 +122,12 @@ class HelixThumb extends Element {
             let currentAlpha = this.currentAlpha || 0;
             currentAlpha = currentAlpha + (this.alpha - currentAlpha) / RODIN.Time.deltaTime();
             const alpha = Math.max(-1, Math.min(currentAlpha, 1));
-            this.object3D.material.opacity = 1 - Math.abs(alpha);
             this.object3D.position.x = 2 * alpha;
             this.object3D.position.z = -Math.abs(alpha);
             this.object3D.rotation.y = -Math.PI / 2 * alpha;
             this.object3D.rotation.x = 0;
             this.currentAlpha = currentAlpha;
-
-            if (this.uv) {
-                let currentUV = this.currentUV || { x: 0, y: 0 };
-                currentUV.x = currentUV.x + (this.uv.x - currentUV.x) / RODIN.Time.deltaTime();
-                currentUV.y = currentUV.y + (this.uv.y - currentUV.y) / RODIN.Time.deltaTime();
-                this.object3D.rotation.y += (currentUV.x - 0.5) / 4;
-                this.object3D.rotation.x += (0.5 - currentUV.y) / 2;
-                this.currentUV = currentUV;
-            }
         });
-
-        this.on(EVENT_NAMES.CONTROLLER_HOVER, (evt) => {
-            this.uv = evt.uv;
-        });
-
-        this.on(EVENT_NAMES.CONTROLLER_HOVER_OUT, () => {
-            this.uv = { x: .5, y: .5 }
-        });
-
-        this.on(EVENT_NAMES.CONTROLLER_KEY_DOWN, (evt) => {
-            this.hasOwnProperty("index") && helix.concentrate(this.index);
-        })
     }
 }
 
