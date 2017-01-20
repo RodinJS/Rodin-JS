49b0d3223c1b16e25451fb253f9fe0f7eac96492
chriskarap <karapetyanchristina@gmail.com>
2017-01-20 18:08:04 +0400

    Drag and Drop with bugs connecting with parent shift

--- src/js/rodinjs/utils/ChangeParent.js
+++ src/js/rodinjs/utils/ChangeParent.js
@@ -7,15 +7,8 @@ import '../../vendor/oimo/oimo.js';
  * @param { THREE.Object3D } targetParent
  */
 export default function changeParent(object = null, targetParent = null) {
-    // TODO change as rodinphysics update
 
     if (object instanceof THREE.Object3D) {
-        /*let globalMatrix = object.matrixWorld;
-        let currParent = object.parent;
-        currParent && currParent.remove(object);
-        targetParent.add(object);
-        setGlobalMatrix(object, globalMatrix);*/
-
         let currParent = object.parent;
         let initPos = object.getWorldPosition();
         let initQuat = object.getWorldQuaternion();
@@ -31,16 +24,4 @@ export default function changeParent(object = null, targetParent = null) {
         object.applyMatrix(new THREE.Matrix4().getInverse(targetParent.matrixWorld));
         targetParent.add(object);
     }
-
-}
-//todo: fix this shit
-function setGlobalMatrix(object, matrix){
-    let inverseParentMatrix = new THREE.Matrix4();
-    let newGlobalMatrix = matrix.clone();
-
-    inverseParentMatrix.getInverse(object.parent.matrixWorld);
-    newGlobalMatrix.multiplyMatrices(inverseParentMatrix, newGlobalMatrix);
-
-    object.matrixAutoUpdate = false;
-    object.matrix = newGlobalMatrix;
 }
