838ed1388a0bc5a693e09e6a8eac4e40267118d8
chriskarap <karapetyanchristina@gmail.com>
2017-01-27 18:35:01 +0400

    basic teleport example

--- examples/BasicTeleport/index.js
+++ examples/BasicTeleport/index.js
@@ -37,87 +37,72 @@ floor.on('ready', (e) => {
     floor.object3D.name = 'floor';
 });
 
-let a = new RODIN.THREEObject(new THREE.Mesh(new THREE.BoxGeometry(2, 1, 2), new THREE.MeshLambertMaterial({color: 0xf15245})));
+let a = new RODIN.THREEObject(new THREE.Mesh(new THREE.BoxGeometry(2, 0.5, 0.5), new THREE.MeshLambertMaterial({color: 0xf15245})));
 a.on('ready', (e) => {
     scene.add(a.object3D);
-    a.object3D.position.set(2, 0.5, 2);
+    a.object3D.position.set(2, 0.5, 0);
     a.raycastable = true;
 });
 
-
-// todo change line points position
+// todo check if raycaster.raycast().length is null always and line didn't change position
+let raycaster = new Raycaster();
+raycaster.setScene(scene.scene);
+let cylinderHeight = 0.3;
+let raycastPoint = new THREE.Mesh(new THREE.CylinderGeometry(0.1, 0.1, cylinderHeight, 10));
+scene.add(raycastPoint);
+let n = 1;
+let g = -9.8;
+let l = 0.01;
 function addPoint(direction) {
+    let points = [];
+    direction = direction.normalize();
+    direction.multiplyScalar(6);
+
+    let i = 0;
+    for (let time = 0; time < n; time += l) {
+        let x = time * (-direction.x);
+        let y = time * (-direction.y) + g * time * time / 2;
+        let z = time * (-direction.z);
+
+        let pointPosition = new THREE.Vector3(x, y, z);
+        points.push(pointPosition);
+        if (i > 0) {
+            raycaster.ray.origin.copy(controllerL.getWorldPosition().add(points[i - 1])); //////////
+            let curVertex = points[i - 1].clone();
+            let nextVertex = points[i].clone();
+            raycaster.ray.direction = nextVertex.sub(curVertex);
+            //nextVertex is now the difference
+            //vector of two points in our line
+            raycaster.far = nextVertex.length();
+            let objs = raycaster.raycast();
+            if (objs.length) {
+                createLine(points);
+                raycastPoint.position.copy(controllerL.getWorldPosition().add(points[i]));
+                console.log(raycaster.far);
+                raycastPoint.position.y += raycaster.far;
+                break;
+            }
+        }
+        ++i;
+    }
+}
+
+function createLine(points) {
     scene.scene.children.map(child => {
         if (child.name == 'line') {
             scene.scene.remove(child);
         }
     });
 
-    let splinepts = [];
-    let n = 1;
-    let g = -9.8;
-    let l = 0.1;
-    direction = direction.normalize();
-    direction.multiplyScalar(2);
-    for (let i = 0; i < n; i += l) {
-        let x = i * Math.sqrt(direction.x * direction.x + direction.z * direction.z);
-        let y = (-direction.y) * i + g * i * i / 2;
-        splinepts.push(new THREE.Vector2(x, y));
-    }
-    let splineShape = new THREE.Shape();
-    splineShape.moveTo(0, 0);
-    splineShape.splineThru(splinepts);
-
-    let points = splineShape.createPointsGeometry();
-    let line = new THREE.Line(points, new THREE.LineBasicMaterial({color: 0x808080}));
+    let splineShape = new THREE.CatmullRomCurve3(points);
+    let tube = new THREE.TubeBufferGeometry(splineShape, 25, 0.01, 4);
+    let line = THREE.SceneUtils.createMultiMaterialObject(tube, [
+        new THREE.MeshLambertMaterial({
+            color: 0x32c5f3
+        })]);
     line.name = 'line';
     line.position.copy(controllerL.getWorldPosition());
-    line.rotation.y = -getAngle(
-        new THREE.Vector2(0, 0),
-        new THREE.Vector2(controllerL.getWorldDirection().x, controllerL.getWorldDirection().z)
-    );
     scene.add(line);
-    createPointsRaycaster(line);
-
-}
-function createPointsRaycaster(line) {
-
-    let vert = line.geometry.vertices;
-    let raycaster = new Raycaster();
-    for (let i = 0; i < vert.length - 1; i++) {
-        raycaster.setScene(scene.scene);
-        raycaster.ray.origin.copy(line.getWorldPosition().add(vert[i]));
-
-        let curVertex = vert[i].clone();
-        let nextVertex = vert[i + 1].clone();
-
-        raycaster.ray.direction = nextVertex.sub(curVertex);
-        //nextVertex is now the difference
-        //vector of two points in our line
-        raycaster.far = nextVertex.length();
-        line.raycaster = raycaster;
-        let objs = raycaster.raycast();
-        if (objs.length) {
-            console.log(objs);
-            break;
-        }
-    }
-}
-
-function dotAngle(a, b) {
-    return Math.acos((a.x * b.x + a.y * b.y) / a.length() / b.length());
-}
-function getAngle(a, b) {
-    a = a.sub(b);
-    if (a.x >= 0 && a.y >= 0) {
-        return dotAngle(a, new THREE.Vector2(1, 0));
-    } else if (a.x >= 0 && a.y < 0) {
-        return Math.PI * 2.0 - dotAngle(a, new THREE.Vector2(1, 0));
-    } else if (a.x < 0 && a.y < 0) {
-        return Math.PI * 2.0 - dotAngle(a, new THREE.Vector2(1, 0));
-    } else {
-        return dotAngle(a, new THREE.Vector2(1, 0));
-    }
 }
 
 controllerL.onKeyDown = controllerKeyDown;
